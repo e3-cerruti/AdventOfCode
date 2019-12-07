ee5fec057497a7af6d0d9b10787adea8500b4d81
Stephen Cerruti <scerruti@e3civichigh.com>
2019-12-07 13:32:56 -0800

    Day 7 Part 1 Complete

--- day_07.py
+++ day_07.py
@@ -1,3 +1,4 @@
+import itertools
 import queue
 
 from intcode_machine import IntcodeMachine
@@ -8,15 +9,14 @@ computers = []
 
 def init_amplifiers():
     global queues, computers
-    queues = [queue.Queue(),queue.Queue(),queue.Queue(),queue.Queue(),queue.Queue(),queue.Queue()]
-    computers = [IntcodeMachine(),IntcodeMachine(),IntcodeMachine(),IntcodeMachine(),IntcodeMachine()]
+    queues = [queue.Queue(), queue.Queue(), queue.Queue(), queue.Queue(), queue.Queue(), queue.Queue()]
+    computers = [IntcodeMachine(), IntcodeMachine(), IntcodeMachine(), IntcodeMachine(), IntcodeMachine()]
     for i in range(5):
-        computers[i].set_queues(queues[i], queues[i+1])
+        computers[i].set_queues(queues[i], queues[i + 1])
 
 
-def compute_signal_strength(phases, file_name):
+def compute_signal_strength(phases, program):
     global queues, computers
-    program = IntcodeMachine.get_program_from_file(file_name)
     for i in range(5):
         queues[i].put(phases[i])
         computers[i].load_program(program)
@@ -27,3 +27,20 @@ def compute_signal_strength(phases, file_name):
 
     return queues[5].get()
 
+
+def main():
+    program = IntcodeMachine.get_program_from_file('day_07.dat')
+    init_amplifiers()
+
+    phase_settings = [0, 1, 2, 3, 4]
+    phase_permutations = list(itertools.permutations(phase_settings))
+    signal = compute_signal_strength(phase_permutations[0], program.copy())
+
+    for phase_array in phase_permutations[1:]:
+        signal = max(signal, compute_signal_strength(phase_array, program.copy()))
+
+    print(signal)
+
+
+if __name__ == '__main__':
+    main()
--- test_compute_signal_strength.py
+++ test_compute_signal_strength.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from day_07 import init_amplifiers, compute_signal_strength
+from intcode_machine import IntcodeMachine
 
 
 class TestComputeSignalStrength(TestCase):
@@ -11,7 +12,8 @@ class TestComputeSignalStrength(TestCase):
     #
     # 3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0
     def test_43210(self):
-        signal = compute_signal_strength([4, 3, 2, 1, 0], 'amp_test_43210.dat')
+        program = IntcodeMachine.get_program_from_file('amp_test_43210.dat')
+        signal = compute_signal_strength([4, 3, 2, 1, 0], program)
         self.assertEqual(43210, signal)
 
     # Max thruster signal 54321 (from phase setting sequence 0,1,2,3,4):
@@ -19,7 +21,8 @@ class TestComputeSignalStrength(TestCase):
     # 3,23,3,24,1002,24,10,24,1002,23,-1,23,
     # 101,5,23,23,1,24,23,23,4,23,99,0,0
     def test_01234(self):
-        signal = compute_signal_strength([0, 1, 2, 3, 4], 'amp_test_01234.dat')
+        program = IntcodeMachine.get_program_from_file('amp_test_01234.dat')
+        signal = compute_signal_strength([0, 1, 2, 3, 4], program)
         self.assertEqual(54321, signal)
 
     # Max thruster signal 65210 (from phase setting sequence 1,0,4,3,2):
@@ -27,5 +30,6 @@ class TestComputeSignalStrength(TestCase):
     # 3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
     # 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0
     def test_65210(self):
-        signal = compute_signal_strength([1, 0, 4, 3, 2], 'amp_test_65210.dat')
+        program = IntcodeMachine.get_program_from_file('amp_test_65210.dat')
+        signal = compute_signal_strength([1, 0, 4, 3, 2], program)
         self.assertEqual(65210, signal)
