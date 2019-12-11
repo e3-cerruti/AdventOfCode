c70d62489b08c4d01ce801f4381e22772fe9dcda
Stephen Cerruti <scerruti@e3civichigh.com>
2019-12-10 21:46:20 -0800

    Day 11 Complete

---
+++ day_11/day_11.py
@@ -0,0 +1,89 @@
+import queue
+from threading import Thread
+
+from intcode_machine import IntcodeMachine
+
+x = y = 0
+direction = 0
+# Start on black
+# hull = {(x, y): 0}
+# Start on white
+hull = {(x, y): 1}
+
+
+def up():
+    global y
+    y += 1
+
+
+def down():
+    global y
+    y -= 1
+
+
+def right():
+    global x
+    x += 1;
+
+
+def left():
+    global x
+    x -= 1
+
+move = {
+    0: up,
+    1: right,
+    2: down,
+    3: left
+}
+
+input_queue = queue.Queue()
+output_queue = queue.Queue()
+computer = IntcodeMachine(input_queue, output_queue)
+computer.load_program_from_file('paint.dat')
+
+
+input_queue.put(hull.get((0, 0)))
+
+computer_thread = Thread(target=computer.run)
+computer_thread.start()
+
+while computer_thread.is_alive():
+    color = output_queue.get()
+    hull[(x, y)] = color
+
+    turn = output_queue.get()
+    if turn:
+        direction = (direction + 1) % 4
+    else:
+        direction = (direction + 3) % 4
+
+    func = move.get(direction, "Nothing")
+    func()
+
+    if (x, y) in hull.keys():
+        input_queue.put(hull[(x, y)])
+    else:
+        input_queue.put(0)
+
+print(len(hull))
+
+min_x = 0
+min_y = 0
+max_x = 0
+max_y = 0
+
+for x, y in hull.keys():
+    min_x = min(min_x, x)
+    min_y = min(min_y, y)
+    max_x = max(max_x, x)
+    max_y = max(max_y, y)
+
+for y in range(max_y, min_y - 1, -1):
+    for x in range(min_x, max_x+1):
+        if (x, y) in hull.keys() and hull.get((x, y)) == 1:
+            print('#', end='')
+        else:
+            print(' ', end='')
+    print()
+
---
+++ day_11/intcode_machine.py
@@ -0,0 +1,112 @@
+class IntcodeMachine:
+    def __init__(self, input_queue=None, output_queue=None):
+        self.halted = False
+        self.instruction_set = {
+            1: [self.add, 3],
+            2: [self.multiply, 3],
+            3: [self.read, 1],
+            4: [self.write, 1],
+            5: [self.jump_if_true, 2],
+            6: [self.jump_if_false, 2],
+            7: [self.less_than, 3],
+            8: [self.equals, 3],
+            9: [self.change_relative_base, 1],
+            99: [self.halt, 0],
+        }
+        self.instruction_pointer = 0
+        self.relative_base = 0
+        self.program = None
+        self.input_queue = input_queue
+        self.output_queue = output_queue
+
+    def set_queues(self, input_queue, output_queue):
+        self.input_queue = input_queue
+        self.output_queue = output_queue
+
+    def load_program(self, program):
+        self.program = {k: v for k, v in enumerate(program)}
+
+    @staticmethod
+    def get_program_from_file(file_name):
+        program = []
+        with open(file_name) as program_file:
+            for line in program_file:
+                program.extend([int(i) for i in line.strip().split(',')])
+        return program
+
+    def load_program_from_file(self, file_name):
+        self.load_program(self.get_program_from_file(file_name))
+
+    def add(self, args):
+        self.program[args[2]] = self.program.get(args[0], 0) + self.program.get(args[1], 0)
+
+    def multiply(self, args):
+        self.program[args[2]] = self.program.get(args[0], 0) * self.program.get(args[1], 0)
+
+    def read(self, args):
+        self.program[args[0]] = self.input_queue.get()
+
+    def write(self, args):
+        self.output_queue.put(self.program.get(args[0], 0))
+
+    def jump_if_true(self, args):
+        if self.program.get(args[0], 0):
+            self.instruction_pointer = self.program.get(args[1], 0)
+
+    def jump_if_false(self, args):
+        if not self.program.get(args[0], 0):
+            self.instruction_pointer = self.program.get(args[1], 0)
+
+    def less_than(self, args):
+        if self.program.get(args[0], 0) < self.program.get(args[1], 0):
+            self.program[args[2]] = 1
+        else:
+            self.program[args[2]] = 0
+
+    def equals(self, args):
+        if self.program.get(args[0], 0) == self.program.get(args[1], 0):
+            self.program[args[2]] = 1
+        else:
+            self.program[args[2]] = 0
+
+    def change_relative_base(self, args):
+        self.relative_base += self.program.get(args[0], 0)
+
+    def halt(self, _1):
+        self.halted = True
+
+    def decode(self, opcode):
+        code = opcode % 100
+        modes = opcode // 100
+        parameter_modes = []
+
+        for i in range(self.instruction_set[code][1]):
+            parameter_modes.append(modes % 10)
+            modes //= 10
+
+        return code, parameter_modes
+
+    def run(self):
+        self.halted = False
+        self.instruction_pointer = 0
+        while not self.halted:
+            opcode, parameter_modes = self.decode(self.program[self.instruction_pointer])
+            parameters = []
+            for mode in parameter_modes:
+                self.instruction_pointer += 1
+                if mode == 1:
+                    parameters.append(self.instruction_pointer)
+                elif mode == 2:
+                    relative_mode_parameter = self.relative_base + self.program.get(self.instruction_pointer, 0)
+                    parameters.append(relative_mode_parameter)
+                else:
+                    parameters.append(self.program[self.instruction_pointer])
+
+            self.instruction_pointer += 1
+            self.instruction_set[opcode][0](parameters)
+
+        return self.program[0]
+
+    def run_program(self, program):
+        self.load_program(program)
+        return self.run()
